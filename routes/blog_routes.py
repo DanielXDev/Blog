facf926c47332a7859243e97a3e91b2ea340b257
DanielXDev <omokoredaniel5@gmail.com>
2025-10-20 23:25:32 +0100

    Blogs and comments delete added

--- routes/blog_routes.py
+++ routes/blog_routes.py
@@ -37,6 +37,30 @@ def create_blog():
         return redirect(url_for("main.homepage"))
     return render_template("create.html", form=form)
 
+@blog_bp.route("/edit/<int:blog_id>", methods=["GET", "POST"])
+@admin_only
+@login_required
+def edit_blog(blog_id):
+    blog = BlogPost.query.get_or_404(blog_id)
+    form = CreateBlogForm(obj=blog)
+    if form.validate_on_submit():
+        blog.title = form.title.data
+        blog.subtitle = form.subtitle.data
+        blog.body = form.body.data
+        blog.author = form.author.data
+        blog.img_url = form.img_url.data
+        db.session.commit()
+        return redirect(url_for("blog.get_blog", title=blog.title))
+    return render_template("edit.html", form=form, blog=blog)   
+
+@blog_bp.route("/delete/<int:blog_id>")
+@admin_only
+@login_required
+def delete_blog(blog_id):
+    blog = BlogPost.query.get_or_404(blog_id)
+    db.session.delete(blog)
+    db.session.commit()
+    return redirect(url_for("main.homepage"))
 
 @blog_bp.route("/blog/<title>", methods=["GET", "POST"])
 @login_required
@@ -52,5 +76,17 @@ def get_blog(title):
         )
         db.session.add(new_comment)
         db.session.commit()
-        return redirect(url_for("blog.get_blog", id=blog.id))
+        return redirect(url_for("blog.get_blog", title=title))
     return render_template("blog.html", blog=blog, form=form, current_user=current_user)
+
+@blog_bp.route("/delete_comment/<int:comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.get_or_404(comment_id)
+    blog_id = comment.post_id
+    blog = BlogPost.query.get_or_404(blog_id)
+    if current_user.id != 1 and current_user.name != comment.name:
+        return abort(403)
+    db.session.delete(comment)
+    db.session.commit()
+    return redirect(url_for("blog.get_blog", title=blog.title)) 
